9311cb65e0f86df6688061fe7f5f7b093913992f
gitmsx <gitmsx@mail.ru>
2022-12-02 14:13:31 +0300

    Saved on 02.12.2022-14:13:31,21

--- Assets/Scripts/CameraMoveYandex.cs
+++ Assets/Scripts/CameraMoveYandex.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraMove : MonoBehaviour
+public class CameraMoveYandex : MonoBehaviour
 {
 
     [SerializeField] private Transform _target;
